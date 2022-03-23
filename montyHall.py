5eb1637bd49fc85d1bd994bfcb0a712d2127a54e
AD1616 <sahilsamar031@gmail.com>
2022-03-22 17:14:52 -0700

    Added randomized computer function to monty and added tester to see 2:1 ratio

--- montyHall.py
+++ montyHall.py
@@ -1,5 +1,7 @@
 import random
 
+win_no_switch = 0
+win_switch = 0
 
 def choose():
     options = [1, 2, 3]
@@ -30,9 +32,40 @@ def choose():
         print("goat")
 
 
-def tester():
+def computer():
+    global win_switch
+    global win_no_switch
+    options = [1, 2, 3]
+    car = random.randint(1,3)
+    for x in options:
+        if x == car:
+            options.remove(x)
+    firstChosen = random.randint(1, 3)
+    decision = random.randint(0,1)
+    if decision == 0:
+        if firstChosen == car:
+            win_no_switch +=1
+    if decision == 1:
+        if firstChosen != car:
+            win_switch +=1
+
+
+def testerChoose():
     choose()
 
 
+def testerComputer():
+    total = 0
+    x = int(input("How many samples do you want to test? "))
+    y = int(input("How large do you want each sample to be? "))
+    for i in range(0, x):
+        for i in range(0, y):
+            computer()
+        print(win_switch, win_no_switch)
+        total += win_switch/win_no_switch
+    average = total/x
+    print("The average ratio of winning with switching to winning without switching was " + str(average))
+
+
 if __name__ == "__main__":
-    tester()
+    testerComputer()
